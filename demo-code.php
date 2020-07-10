6c5e6aac732be8ef1c5ed7679f6dee60008374d6
Eleas Kanchon <44223562+Eleaswebdev@users.noreply.github.com>
2020-07-10 14:10:48 +0600

    Create demo-code.php

---
+++ demo-code.php
@@ -0,0 +1,42 @@
+<div class="row">
+		<?php
+		// Get all our posts, and start the counter
+            $postNumber = 0;
+            $args = array(
+                'posts_per_page' => 20
+            );
+            $posts = get_posts($args);
+            // Loop through each of our posts
+            foreach ($posts as $post) {
+                // If we're at the first post, or just before a post number that is divisible by three then we start a new row
+                // if (($postNumber == 0) || (($postNumber+1) % 3 == 0)) {
+                //     echo '<div class="row">';
+                // }
+                // Choose the post class based on what number post we are 
+
+                $col_width = '';
+                 if('0'==($postNumber%5) || '0'==(($postNumber-1)%5)){
+                $col_width="col-md-6";
+                }else {
+                    $col_width .= "col-md-4";
+                }
+
+               // $col_width="col-md-4";
+               //  if('0'==($postNumber%5) || '0'==(($postNumber-1)%5)){
+               //  $col_width="col-md-6";
+               //  }
+
+                
+                echo '<div class="'.$col_width.'">';
+                // Print the post data...
+                echo $postNumber. " " . $col_width;
+                echo "</div>";
+                // If we are at the second post, or we're just after a post number divisible by three we are at the end of the row
+                // if (($postNumber == 1) || (($postNumber-1) % 3 == 0)) {
+                //     echo '</div>'; // close row tag
+                // }
+                $postNumber++; // Increment counter
+            }
+		?>
+
+       </div>
